0448f5f8400056ab46ab8c7c122fa41eef3d205d
Ian Freislich <ianf@freislich.nom.za>
2023-09-29 07:20:06 -0400

    The hostname has changed, reflect this is the upload script. My last partition table caused a boot loop after crash no matter what   firmware was subsequently uploaded. I think that the boot loader doesn't   like the OTA partition at the end of flash so move NVS to the end and   place otadata near the start.

--- src/upload.sh
+++ src/upload.sh
@@ -2,5 +2,5 @@ ESPOTA=/home/ianf/.platformio/packages/framework-arduinoespressif32/tools/espota
 
 for i in aviwe catkin fred gobbolino oliver sophie
 do
-    python3 ${ESPOTA} -r -i catfeeder-$i.lan -p 3232 --auth= -f ../.pio/build/esp32dev/firmware.bin
+    python3 ${ESPOTA} -r -i feeder-$i.lan -p 3232 --auth= -f ../.pio/build/esp32dev/firmware.bin
 done
